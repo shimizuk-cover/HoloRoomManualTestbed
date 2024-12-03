68af54f1f19942c38bb18eae050786769ab8d295
ShimizuKenjiro <129722191+shimizuk-cover@users.noreply.github.com>
2024-12-03 17:00:49 +0900

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -18,7 +18,7 @@ const config = {
   url: 'https://shimizuk-cover.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/HoloRoomTestbed/',
+  baseUrl: '/HoloRoomManualTestbed/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
